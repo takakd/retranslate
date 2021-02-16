be4ff4bae34c081f78a3024c79d2396bae68af2a
Takahiro Kudo <13297326+takakd@users.noreply.github.com>
2021-02-16 15:41:32 +0900

    refactor: several items (#10)
    - Change environment variable names
    - Refactor GitHub Actions
    - Update readme
    - Refactor proto script

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,4 @@
+// This files is used to enable to TypeScript by Next.js
+// Ref. https://nextjs.org/docs/basic-features/typescript
 /// <reference types="next" />
 /// <reference types="next/types/global" />
